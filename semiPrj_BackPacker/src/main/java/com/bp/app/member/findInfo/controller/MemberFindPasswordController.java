813770969cf6f73b19ba2ff8673d3ee54d556846
seyeon ji <joeun9705@gmail.com>
2023-05-10 21:13:51 +0900

    커밋 : 비밀번호 찾기 화면 구현

---
+++ semiPrj_BackPacker/src/main/java/com/bp/app/member/findInfo/controller/MemberFindPasswordController.java
@@ -0,0 +1,17 @@
+package com.bp.app.member.findInfo.controller;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/member/find/password")
+public class MemberFindPasswordController extends HttpServlet {
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		req.getRequestDispatcher("/WEB-INF/views/member/findPasswordForm.jsp").forward(req, resp);
+	}
+}
