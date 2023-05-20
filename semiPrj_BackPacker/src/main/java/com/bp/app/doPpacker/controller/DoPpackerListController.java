2ff7a9f855b1a25b685895d86994e266e4b438f1
ehatchu <yer5_27@naver.com>
2023-05-21 00:35:03 +0900

    어노테이션 주소수정 Ppacker->Fpacker

--- semiPrj_BackPacker/src/main/java/com/bp/app/doPpacker/controller/DoPpackerListController.java
+++ semiPrj_BackPacker/src/main/java/com/bp/app/doPpacker/controller/DoPpackerListController.java
@@ -8,7 +8,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@WebServlet ("/doPpacker/list")
+@WebServlet ("/doFpacker/list")
 public class DoPpackerListController extends HttpServlet{
 	@Override
 	protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
--- semiPrj_BackPacker/src/main/java/com/bp/app/doPpacker/controller/DoPpackerWriteController.java
+++ semiPrj_BackPacker/src/main/java/com/bp/app/doPpacker/controller/DoPpackerWriteController.java
@@ -8,7 +8,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@WebServlet ("/doPpacker/write")
+@WebServlet ("/doFpacker/write")
 public class DoPpackerWriteController extends HttpServlet{
 	@Override
 	protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
