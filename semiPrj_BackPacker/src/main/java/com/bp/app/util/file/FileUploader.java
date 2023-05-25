b615d0973ab321479b0d8f49cb23667ae48af62b
윤예린 <yer5_27@naver.com>
2023-05-25 12:07:01 +0900

    파일업로더 수정

--- semiPrj_BackPacker/src/main/java/com/bp/app/util/file/FileUploader.java
+++ semiPrj_BackPacker/src/main/java/com/bp/app/util/file/FileUploader.java
@@ -4,11 +4,24 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.UUID;
 
 import javax.servlet.http.Part;
 
 public class FileUploader {
+	
+	public static List<AttachmentVo> saveFile(String path, List<Part> fList) throws Exception{
+		List<AttachmentVo> list = new ArrayList<>();
+		
+		for(Part f : fList) {
+			AttachmentVo attVo = saveFile(path,f);
+			list.add(attVo);
+		}
+		return list;
+		
+	}
 	
 	public static AttachmentVo saveFile(String path , Part f) throws Exception {
 		String randomName = UUID.randomUUID().toString();
